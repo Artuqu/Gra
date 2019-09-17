89571f8913d5b936af92f7eb15b7d1c73f531201
Artuqu <percikartur@gmail.com>
2019-09-17 17:41:23 +0200

    ver.0

---
+++ GraZaDuzoZaMalo/Gra/GraLib/ModelGry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraLib
+{
+    public class ModelGry
+    {
+        private int wylosowana;                         /*int wylosowana: 72 zakresOd 0 do 100 historia gry -> czas, propozycja, odpowiedź komputera*/
+        public int ZakresOd { get; private set; }
+        public int ZakresDo { get; private set; }
+
+        public ModelGry(int zakresOd, int zakresDo)
+        {
+            ZakresOd = zakresOd; //duże litery unikają funkcji this, funkcja będzie dla tych liczb publiczna
+            ZakresDo = zakresDo;
+            Random los = new Random();
+            wylosowana = los.Next(ZakresOd, ZakresDo + 1);
+
+        }
+        
+    }
+}
